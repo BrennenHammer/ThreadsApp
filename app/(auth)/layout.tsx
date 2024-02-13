52501ab1f5346678f819fcf13480efdb4df45bdb
Test <Hammertyme101@gmail.com>
2024-02-12 23:21:34 -0500

    commitlayout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+export const metadata = {
+title: 'threads',
+description: 'A Next.js 13 meta Threads Application'
+
+}
+
+export default function rootlayout({ children }: {children: React.ReactNode }){
+
+}
